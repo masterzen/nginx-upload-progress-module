e863ac7928b7001438ea975b207719c8cd084311
Brice Figureau <brice@daysofwonder.com>
2008-09-27 23:16:29 +0200

    Attempt to fix nginx upload module behavior that sends a POST after the upload is finished, which fools our tracker.

--- ngx_http_uploadprogress_module.c
+++ ngx_http_uploadprogress_module.c
@@ -365,8 +365,8 @@ static void ngx_http_uploadprogress_event_handler(ngx_http_request_t *r)
     if (up != NULL && !up->done) {
         ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
                        "upload-progress: read_event_handler found node: %V", id);
-        up->rest = r->request_body->rest;
-        up->length = r->headers_in.content_length_n;
+        up->rest = up->rest >= r->request_body->rest ? r->request_body->rest : up->res;
+        up->length = up->length == 0 ? r->headers_in.content_length_n : up->length;
         ngx_log_debug3(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
                        "upload-progress: read_event_handler storing rest %uO/%uO for %V", up->rest, up->length, id);
     } else {
